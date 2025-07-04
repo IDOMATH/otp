056263208f8c31b2f2d8778e198a72fa66783ff9
IDOMATH <btdothemath@gmail.com>
2025-07-04 14:43:10 -0500

    Getting ready for when base_change.ToTen is finished

--- main.go
+++ main.go
@@ -3,14 +3,19 @@ package main
 import (
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
+	"strings"
+
+	"github.com/IDOMATH/CheetahMath/base_change"
 )
 
 func main() {
 	fmt.Println("Hello world!")
 	serverPort := "8080"
 	router := http.NewServeMux()
-	router.HandleFunc("/", handleHome)
+	router.HandleFunc("GET /", handleHome)
+	router.HandleFunc("GET /otp", sendOtp)
 	server := http.Server{
 		Addr:    fmt.Sprint(":", serverPort),
 		Handler: router,
@@ -27,6 +32,17 @@ func sendOtp(w http.ResponseWriter, r *http.Request) {
 	// find a way to make a unique id.  Something with the user email
 	// add otp and id to database
 	// send email
+	w.Write([]byte(MakeOtp(8)))
+}
+
+func MakeOtp(len int) string {
+	otp := strings.Builder{}
+	r := rand.New(rand.NewSource(99))
+	for i := 0; i < len; i++ {
+		fmt.Println("Random: ", r.Int63n(36))
+		otp.Write([]byte(base_change.FromTen(36, r.Int63n(36))))
+	}
+	return otp.String()
 }
 
 func checkOtp(w http.ResponseWriter, r *http.Request) {
---
+++ main_test.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestMakeOtp(t *testing.T) {
+	fmt.Println("TEsting")
+	fmt.Println(MakeOtp(8))
+}
