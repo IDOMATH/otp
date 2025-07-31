a1d1c3db572fe83034c4f10eb4aca9c9510c9c8a
IDOMATH <btdothemath@gmail.com>
2025-07-30 22:15:29 -0500

    More db stuff

--- db/otp_store.go
+++ db/otp_store.go
@@ -1,6 +1,10 @@
 package db
 
-import "database/sql"
+import (
+	"context"
+	"database/sql"
+	"time"
+)
 
 type OtpStore struct {
 	Db *sql.DB
@@ -10,4 +14,13 @@ func NewOtpStore(db *sql.DB) *OtpStore {
 	return &OtpStore{
 		Db: db,
 	}
+}
+
+func (s *OtpStore) InsertOtp(otp string) int {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
+	defer cancel()
+
+	statement := `INSERT INTO otp (password) values ($1)`
+
+	return 0
 }
